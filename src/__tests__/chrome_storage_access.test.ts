557c54e956c84931a51e162d829cdda3574d336d
da-mizuno <daichi.mizuno.0910@gmail.com>
2021-11-03 17:22:13 +0900

    タブの色を保存するように実装

--- src/__tests__/chrome_storage_access.test.ts
+++ src/__tests__/chrome_storage_access.test.ts
@@ -1,32 +1,70 @@
 import ChromeStorageAccess, { TabGroup } from "../chrome_storage_access";
+import { chrome } from "jest-chrome";
+
+let store = {} as any;
+const storage = new ChromeStorageAccess();
+
+const mockGetSync = chrome.storage.sync.get;
+const mockSetSync = chrome.storage.sync.set;
+
+const mockGet = (objToGet: any, callback: any) => {
+  console.log(`before objToGET : ${objToGet} , store : ${JSON.stringify(JSON.stringify(store))}`)
+
+  let defaultValue: any
+  switch(String(Object)){
+    case "tabGroupLastIndex":
+      defaultValue = NaN
+    case "tabGroup":
+      defaultValue = []
+  }
+
+  let res = store[objToGet] === undefined ? { [objToGet]: defaultValue } : { [objToGet]: store[objToGet] };
+
+  callback(res);
+};
+const mockSet = (objToSet: any, callback: any) => {
+  store = {
+    ...store,
+    ...objToSet,
+  };
+  callback();
+};
+
+mockGetSync.mockImplementation(mockGet);
+mockSetSync.mockImplementation(mockSet);
+
+beforeEach(() => {
+  store = {};
+  mockGetSync.mockClear();
+  mockSetSync.mockClear();
+});
 
 describe("ChromeStorageAccessのテスト", () => {
-  describe('addNewGroup', () => {
-    test("新しいグループが追加できる関数を呼ぶと、syncUpdateLastIndexとgetAllTabGroupが１度ずつ呼ばれること", async () => {
-      const storage = new ChromeStorageAccess();
-      const existTabGruop = [{ id: 1, tabGroupName: "test" }] as TabGroup[];
-      const newTabGruop = { id: 2, tabGroupName: "test2" } as TabGroup;
-      
-      const mockSyncUpdateLastIndex = jest.spyOn(storage, "syncUpdateLastIndex");
-      const mockGetAllTabGroup = jest.spyOn(storage, "getAllTabGroup");
-      
-      mockSyncUpdateLastIndex.mockImplementation((): Promise<number> => {
-        return new Promise((resolve, reject) => {
-          resolve(1);
-        });
-      });
-  
-      mockGetAllTabGroup.mockImplementation((): Promise<TabGroup[]> => {
-        return new Promise((resolve, reject) => {
-          resolve(existTabGruop);
-        });
-      });
-  
-      await storage.addNewGroup(newTabGruop["tabGroupName"] as string);
-     
-      expect(mockSyncUpdateLastIndex).toBeCalledTimes(1);
-      expect(mockGetAllTabGroup).toBeCalledTimes(1);
-      
+  describe("addNewGroup", () => {
+    test("addNewTabGroup関数を呼ぶと、既にstorageに何もない場合、新しくデータが作成されること", async () => {
+      await storage.addNewTabGroup("test", "red");
+
+      const expcetStorageData = {
+        tabGroup: [{ id: 1, tabColor: "red", tabGroupName: "test" }],
+        tabGroupLastIndex: "1",
+      };
+      expect(store).toEqual(expcetStorageData);
+    });
+
+    test("addNewTabGroup関数を呼ぶと、既にstorageにデータがある場合、新しくデータが作成されること", async () => {
+      await storage.addNewTabGroup("test", "red");
+
+      expect(store).toEqual({ tabGroup: [{ id: 1, tabColor: "red", tabGroupName: "test" }], tabGroupLastIndex: "1" });
+      await storage.addNewTabGroup("test2", "grey");
+
+      const expcetStorageData = {
+        tabGroup: [
+          { id: 1, tabColor: "red", tabGroupName: "test" },
+          { id: 2, tabColor: "grey", tabGroupName: "test2" },
+        ],
+        tabGroupLastIndex: "2",
+      };
+      expect(store).toEqual(expcetStorageData);
     });
   });
 });
--- src/chrome_storage_access.ts
+++ src/chrome_storage_access.ts
@@ -1,38 +1,73 @@
 export interface TabGroup {
   id: number;
   tabGroupName: string;
+  tabColor: Color;
 }
 
+const Color = {
+  Grey: "grey",
+  Blue: "blue",
+  Red: "red",
+  Yellow: "yellow",
+  Green: "green",
+  Pink: "pink",
+  Purple: "purple",
+  Cyan: "cyan",
+} as const;
+export type Color = typeof Color[keyof typeof Color];
+
 export default class ChromeStorageAccess {
-  
-  syncUpdateLastIndex = (): Promise<number> => {
+  addNewTabGroup = async (tabName: string, tabColor: Color) => {
+    const tabGroupLastIndex = ((await this.syncUpdateLastIndex()) as number) || 1;
+    const tabGroupData = ((await this.getAllTabGroup()) as TabGroup[]) || [];
+
+    const newTabGroup = {
+      id: tabGroupLastIndex,
+      tabGroupName: tabName,
+      tabColor: tabColor,
+    } as TabGroup;
+
+    tabGroupData.push(newTabGroup);
+
+    await chrome.storage.sync.set({ tabGroup: tabGroupData }, async () => {
+      console.log(`new tab group is set, name:${tabName}`);
+    });
+  };
+
+  syncUpdateLastIndex = async (): Promise<number> => {
+    let index = await this.getTabGroupLastIndex();
+    if (isNaN(index)) {
+      index = 1;
+    } else {
+      index = index + 1;
+    }
+
+    await this.setTabGroupLastIndex(index.toString());
+    return index;
+  };
+
+  getTabGroupLastIndex = (): Promise<number> => {
     return new Promise((resolve, reject) => {
-      chrome.storage.sync.get(["tabGroupLastIndex"], async (value) => {
+      chrome.storage.sync.get(["tabGroupLastIndex"], (value) => {
         if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError);
-        }
-        let index = Number(value.tabGroupLastIndex);
-        console.log(`default value is ${index}`);
-        if (isNaN(index)) {
-          index = 1;
-        } else {
-          index = index + 1;
+          throw chrome.runtime.lastError;
         }
 
-        await this.setTabGroupLastIndex(index.toString());
-        resolve(index as number);
+        const index = value.tabGroupLastIndex === undefined ? NaN : Number(value.tabGroupLastIndex);
+        resolve(index);
       });
     });
   };
 
-  setTabGroupLastIndex = (newIndex: string) => {
-    console.log("setTabGroupLastIndex is called");
-    chrome.storage.sync.set({ tabGroupLastIndex: newIndex }, () => {
-      if (chrome.runtime.lastError) {
-        console.log(`set tab group last index function is runtime error`);
-      } else {
-        console.log(`new index is set`);
-      }
+  setTabGroupLastIndex = (newIndex: string): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      chrome.storage.sync.set({ tabGroupLastIndex: newIndex }, () => {
+        if (chrome.runtime.lastError) {
+          throw chrome.runtime.lastError;
+        }
+
+        resolve();
+      });
     });
   };
 
@@ -45,23 +80,6 @@ export default class ChromeStorageAccess {
 
         resolve(value.tabGroup as TabGroup[]);
       });
-    });
-  };
-
-  addNewGroup = async (tabName: string) => {
-    const tabGroupLastIndex =
-      ((await this.syncUpdateLastIndex()) as number) || 1;
-    const tabGroupData = ((await this.getAllTabGroup()) as TabGroup[]) || [];
-
-    const newTabGroup = {
-      id: tabGroupLastIndex,
-      tabGroupName: tabName,
-    } as TabGroup;
-
-    tabGroupData.push(newTabGroup);
-
-    chrome.storage.sync.set({ tabGroup: tabGroupData }, async () => {
-      console.log(`new tab group is set, name:${tabName}`);
     });
   };
 }
