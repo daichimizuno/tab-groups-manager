7ab6d019804c7404a0c2efeb9d68c2d742dd3a19
daichimizuno <daichi.mizuno.0910@gmail.com>
2021-12-16 00:31:14 +0900

    ポップアップタブで現在開いているタブを表示するようにした

--- src/pages/components/popup_tabview.tsx
+++ src/pages/components/popup_tabview.tsx
@@ -1,15 +1,27 @@
+import React, { useEffect, useState } from "react";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
-import * as React from "react";
+import OpenedTabView from "./opened_tabs_view";
+import { getAllInWindow } from "../../utils/chrome_tab_utils/chrome_tab_background_worker";
 
 const PopupTabView = () => {
-  const [value, setValue] = React.useState("1");
+  const [tabContextValue, setTabContextValue] = useState("1");
+  const [openedTabs, setOpenedTabs] = useState<chrome.tabs.Tab[]>([]);
+
+  useEffect(() => {
+    getTabs();
+  }, [openedTabs]);
+
+  const getTabs = async () => {
+    const tabs = await getAllInWindow();
+    setOpenedTabs(tabs);
+  };
 
   const handleChange = (event: any, newValue: string) => {
-    setValue(newValue);
+    setTabContextValue(newValue);
   };
 
   const openOptionPage = () => {
@@ -21,7 +33,7 @@ const PopupTabView = () => {
   return (
     <>
       <Box sx={{ width: "400px", typography: "body1" }}>
-        <TabContext value={value}>
+        <TabContext value={tabContextValue}>
           <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
             <TabList
               onChange={handleChange}
@@ -35,7 +47,13 @@ const PopupTabView = () => {
               <Tab label="保存されている設定" value="2" />
             </TabList>
           </Box>
-          <TabPanel value="1">Item One</TabPanel>
+          <TabPanel value="1">
+            {openedTabs.length > 0 ? (
+              <OpenedTabView openedTabs={openedTabs} />
+            ) : (
+              <></>
+            )}
+          </TabPanel>
           <TabPanel value="2">
             <button onClick={openOptionPage}>setting page</button>
           </TabPanel>
