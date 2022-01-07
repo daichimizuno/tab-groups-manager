a61eb2fe996157d8dd1f08608a5fff5cbecf376b
da-mizuno <da-mizuno@kddi.com>
2022-01-08 02:34:13 +0900

    個別のタブグループを開けるようにした

--- src/utils/chrome_tab_utils/chrome_tab_send_message.ts
+++ src/utils/chrome_tab_utils/chrome_tab_send_message.ts
@@ -4,4 +4,9 @@ export default class ChromeTabSendMessage {
   createTabGroupsSendMessage = (tabGroup: TabGroup[]) => {
     chrome.runtime.sendMessage(tabGroup);
   };
+
+  createTabGroupSendMessage = (tabGroup: TabGroup) => {
+    const tabGroups: TabGroup[] = [tabGroup];
+    this.createTabGroupsSendMessage(tabGroups);
+  };
 }
